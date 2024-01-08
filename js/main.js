4b905738f60262355c83330d4b28dc26348ebdda
braverokmc79 <braverokmc79@gmail.com>
2024-01-08 14:17:00 +0900

    15.순차적 애니메이션-순차적으로 요소 보이기

--- js/main.js
+++ js/main.js
@@ -46,3 +46,13 @@ window.addEventListener("scroll", _.throttle(function(){
 }, 500));
 //_.throttle(함수,시간)
 
+const fadeEls=document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+
+      //gsap.to(요소, 지속시간,옵션);
+    gsap.to(fadeEl, 1, {
+        delay:(index+1)*.7,
+        opacity:1,
+    });
+
+});
